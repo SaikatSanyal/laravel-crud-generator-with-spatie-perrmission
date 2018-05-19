be527504b339fc5d20e693de961c7a3af424a7ed
Saikat Sanyal <saikat.ruet12@gmail.com>
2018-05-19 10:55:17 +0600

    first commit

---
+++ app/Http/Controllers/Admin/removePermissionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+
+
+class removePermissionController extends Controller
+{
+
+
+
+    
+}
---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,32 @@
+<div class="col-md-3">
+    <div class="card">
+        <div class="card-header">
+            Sidebar
+        </div>
+
+        <div class="card-body">
+            <div>
+            <ul class="nav" role="tablist">
+                <li role="presentation">
+                    <a href="{{ url('/admin') }}">
+                        Dashboard
+                    </a>
+                </li>
+            </div>
+
+<div>
+                <li role="presentation">
+                    <a href="{{ url('/admin/role') }}">
+                        Role
+                    </a>
+                </li>
+</div>
+                <li role="presentation">
+                    <a href="{{ url('/admin/permission') }}">
+                     Permission
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
